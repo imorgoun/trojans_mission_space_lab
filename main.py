8748a398f23967d0234d906bbbdf350bc101db04
Illya Morgun <116104698+imorgoun@users.noreply.github.com>
2024-01-25 13:26:35 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,34 @@
-# Import all necessary libraries
+# import all necessary libraries
 from picamera import PiCamera
 from time import sleep
+from exif import Image
+from datetime import datetime
 
-# Variables
+# variables
 num_pics = 3
-
 
 # Setting up camera
 cam = PiCamera()
 cam.resolution = (4056, 3040)
 
-# Taking 4 pictures with equal intervals
+# taking pictures with equal intervals
 cam.capture("image0.png")
 for i in range(num_pics-1):
     sleep(60 * (9/(num_pics-1)))
     cam.capture(f"image{i+1}.png")
 cam.close()
+
+# function for returning time picture was taken
+def get_time(image):
+    with open(image, 'rb') as image_file:
+        img = Image(image_file)
+        time_str = img.get("datetime_original")
+        time = datetime.strptime(time_str, '%Y:%m:%d %H:%M:%S')
+    return time
+
+# funtion for returning time diffrence between when two images were taken
+def get_time_difference(image_1, image_2):
+    time_1 = get_time(image_1)
+    time_2 = get_time(image_2)
+    time_difference = time_2 - time_1
+    return time_difference.seconds
